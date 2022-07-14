cc558b0680f87f97cc220dcc87854693092843bd
milad88884 <interceptors8888@gmail.com>
2022-07-14 16:48:02 +0200

    base structure of the project

---
+++ src/assets/data/questionsList.js
@@ -0,0 +1,522 @@
+export const filmQuiz = [
+    {
+        question: "Who is the main protagonist of Top Gun: Maverick?",
+        id: 1,
+        options: [
+            {
+                answer: 'Steve Carell',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Tom Cruise',
+                correct: true,
+                id: 22,
+            },
+            {
+                answer: 'Jean-Claude Van Damme',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'James P. Sullivan',
+                correct: false,
+                id: 44,
+            },
+        ]
+    },
+    {
+        question: "Which option is a quote from the Fast and Furious franchise?",
+        id: 2,
+        options: [
+            {
+                answer: 'What a lame franchise',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Vin Diesel was born to be bald',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Ride or die',
+                correct: true,
+                id: 22,
+            },
+            {
+                answer: 'Die or ride',
+                correct: false,
+                id: 44,
+            },
+        ]
+    },
+    {
+        question: "Were Ross and Rachel on a break in Friends TV Series?",
+        id: 3,
+        options: [
+            {
+                answer: 'How dare u ask that',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'They were on a breaaaaak',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Not really',
+                correct: false,
+                id: 22,
+            },
+            {
+                answer: 'There is no clear answer to this question to this day',
+                correct: true,
+                id: 44,
+            },
+        ]
+    },
+    {
+        question: "Is Interstellar movie about interstellar travel?",
+        id: 4,
+        options: [
+            {
+                answer: 'No, it is about environmental problems',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Yes, it mostly is',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'No, it is about Star Wars',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'No, it is about Michael Caine being old',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "What is the complete name of the Thor movie released in the year 2022?",
+        id: 5,
+        options: [
+            {
+                answer: 'Thor: Love and I can not be bothered',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Thor: Love and Death ',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Thor: Love and Thunder',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'Thor: Love and The Douchebag',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "Which movie below is based on a novel?",
+        id: 6,
+        options: [
+            {
+                answer: 'The Green Mile',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'Toy story 4',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Pitch perfect',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Ted',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "What did the movie I Am Mother focus on?",
+        id: 7,
+        options: [
+            {
+                answer: 'A mother',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Artificial intelligence',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'War',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Climate change',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "Who is batman?",
+        id: 8,
+        options: [
+            {
+                answer: 'Superman',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'I think he is Harley Quinn',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'He is a horse actually',
+                correct: false,
+                id: 22,
+            },
+            {
+                answer: 'A man dressed as a bat',
+                correct: true,
+                id: 44,
+            },
+        ]
+    },
+    {
+        question: "Was lightning McQueen the fastest car in the Cars movie released in the year 2006?",
+        id: 9,
+        options: [
+            {
+                answer: 'No, actually he was the fastest moron',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Yes he was',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'No, Not as fast as Doc Hudson',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'No, Chick Hicks was',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "Hugh Jackman is most famous for what exactly?",
+        id: 10,
+        options: [
+            {
+                answer: 'Playing as Wolverine',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'Being a good boy',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Playing as Darth vader',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Being Australian',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "Which movie is not a sci-fi film?",
+        id: 11,
+        options: [
+            {
+                answer: 'Road To Perdition',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'Dune',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Chappie',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: '12 Monkeys',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "Which one in not a member of The Simpson family?",
+        id: 12,
+        options: [
+            {
+                answer: 'Homer',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Marge',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'Moe',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Lisa',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "Which one is a famous quote from the joker?",
+        id: 13,
+        options: [
+            {
+                answer: 'All you care about is chicken nuggies',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Let us put a smile on that face',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Why so serious',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'I love you batman',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "Did they save Mark Watney at the end of The Martian?",
+        id: 14,
+        options: [
+            {
+                answer: 'No, they left him to rot there',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'He was high, so he was not on mars , just high',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'No they hated him',
+                correct: false,
+                id: 22,
+            },
+            {
+                answer: 'Yes, yes they did',
+                correct: true,
+                id: 44,
+            },
+        ]
+    },
+    {
+        question: "Did you like Scarlett Johansson in The Prestige?",
+        id: 15,
+        options: [
+            {
+                answer: 'No, I hate her in general',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'meh...',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Yes, Chose this option if you want to win the game',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'No, the hell with her',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "Which movie did tom cruise not star in?",
+        id: 16,
+        options: [
+            {
+                answer: 'Mission: Impossible 1',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Mission: Impossible 2',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Mission: Possible 4',
+                correct: false,
+                id: 22,
+            },
+            {
+                answer: 'Mission: Impossible 3',
+                correct: true,
+                id: 44,
+            },
+        ]
+    },
+    {
+        question: "Chris Evans is most famous for playing as:",
+        id: 17,
+        options: [
+            {
+                answer: 'Captain America',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'Captain Italia',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Hot guy in in The Nanny Diaries',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Some lame movie I do not even remember',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "Who is more famous , Chris Hemsworth or Me?",
+        id: 18,
+        options: [
+            {
+                answer: 'Omg, you are',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Chris, Chris is more famous buddy',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'Takes courage to ask such thing',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'You can not be serious?',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "So , Don't Look Up , what is it about?",
+        id: 19,
+        options: [
+            {
+                answer: 'It is about being bored me to death',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Blah blah',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'It is more than just an asteroid heading towards earth',
+                correct: true,
+                id: 44,
+            },
+            {
+                answer: 'meh...',
+                correct: false,
+                id: 22,
+            },
+        ]
+    },
+    {
+        question: "Which one is not a comic character?",
+        id: 20,
+        options: [
+            {
+                answer: 'Bruce wayne',
+                correct: false,
+                id: 11,
+            },
+            {
+                answer: 'Selina kyle',
+                correct: false,
+                id: 33,
+            },
+            {
+                answer: 'Lex luthor',
+                correct: false,
+                id: 22,
+            },
+            {
+                answer: 'The real slim shady',
+                correct: true,
+                id: 44,
+            },
+        ]
+    },
+]
